3d5b4fbe80245a6305dfb02d909b53870c30dfc8
DukeofNeptune <dallin.duke4@gmail.com>
2023-02-11 20:09:06 -0700

    Add project files.

---
+++ Mission06_duke95/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_duke95.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_duke95.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext StoreContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext TempContext)
+        {
+            _logger = logger;
+            StoreContext = TempContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return RedirectPermanent("https://baconsale.com/");
+        }
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(AddMovieResponse amr)
+        {
+            StoreContext.Add(amr);
+            StoreContext.SaveChanges();
+            return View("Index", amr);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_duke95/Models/MovieContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_duke95.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            // Leave blank
+        }
+
+        public DbSet<AddMovieResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AddMovieResponse>().HasData(
+                new AddMovieResponse
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "The Dark Knight",
+                    Year = 2008,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    Lent = "",
+                    Notes = ""
+                },
+                 new AddMovieResponse
+                 {
+                     MovieId = 2,
+                     Category = "Action/Adventure",
+                     Title = "Lord of the Rings: The Two Towers",
+                     Year = 2002,
+                     Director = "Peter Jackson",
+                     Rating = "PG-13",
+                     Edited = false,
+                     Lent = "",
+                     Notes = ""
+                 },
+                  new AddMovieResponse
+                  {
+                      MovieId = 3,
+                      Category = "Family",
+                      Title = "The Emperor's New Groove",
+                      Year = 2000,
+                      Director = "Mark Dindal",
+                      Rating = "G",
+                      Edited = false,
+                      Lent = "",
+                      Notes = ""
+                  }
+                );
+        }
+    }
+}
